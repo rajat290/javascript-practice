d55be4aa4db2494675e7bf9480a1519fa01026b1
rajat <rajat290@users.noreply.github.com>
2025-04-26 22:54:27 +0530

    add content for Optional Chaining in JavaScript, including definition, syntax, practical examples, and interview tips

---
+++ 01. MASTERING JAVSCRIPT FROM SCRATCH/01. JS BASICS/05. Control Flow in JS/5.1 practical.js
@@ -0,0 +1,11 @@
+let user = {
+    profile: {
+
+    name: "Rajat Tomar",
+    age: 30,
+    isAdmin: false,
+    courses: ["HTML", "CSS", "JavaScript"],
+    "likes birds": true,
+}
+}
+console.log(user.profile.name);
