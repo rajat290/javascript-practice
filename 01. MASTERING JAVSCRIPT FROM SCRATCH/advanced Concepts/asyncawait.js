cf3359bfeaff9a94578b60881bea7e4332e26901
rajat <rajat290@users.noreply.github.com>
2025-07-13 22:12:06 +0530

    fix fetchdata function error handling and improve response validation

--- 01. MASTERING JAVSCRIPT FROM SCRATCH/advanced Concepts/asyncawait.js
+++ 01. MASTERING JAVSCRIPT FROM SCRATCH/advanced Concepts/asyncawait.js
@@ -1,12 +1,17 @@
 async function fetchdata(url){
     try{
         const response = await fetch(url);
-        if (!response.ok){
-            throw new Error('network response was not ok' + response.statusText);
-            const data = await response.json();
-            console.log (data);
-            catch(console.error();
-            )
-        }
+       try {
+         if (!response.ok){
+             throw new Error('network response was not ok' + response.statusText);
+             const data = await response.json();
+             console.log (data);
+           
+            }
+       } catch (error) {
+           console.error('There has been a problem with your fetch operation:', error);
+       }       
+    } catch (error) {
+        console.error('There has been a problem with your fetch operation:', error);
     }
 }
